c28ac63971a59c9e19d6120f15680185117845f0
Kyle <ksnavely@localhost.localdomain>
2012-11-06 20:57:07 -0500

    Refactoring Events

--- src/base/Events.py
+++ src/base/Events.py
@@ -1,6 +1,16 @@
+"""
+  Events Module
+  
+    A key goal of this code is to demonstrate the power of object-oriented design.
+  A part of this is the use of the Mediator/Observer Pattern in which objects
+  communicate in one directional, or one to many object Events.
+    Care should be taken not to overburder iterating through collections with
+  messages. Perhaps a direct function call is in order?
+"""
+
 class Event:
   def __init__(self):
-    self.name = "Default Event Name"
+    self.name = "Event"
 
   def getName(self):
     return self.name
