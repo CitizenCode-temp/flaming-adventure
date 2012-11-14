8777c9e17ccfc0f077c5211f6e7ceb6549b792ea
Kyle <ksnavely@localhost.localdomain>
2012-11-14 07:06:45 -0500

    unittesting Models.py

--- src/base/Models_test.py
+++ src/base/Models_test.py
@@ -1,11 +1,14 @@
+# Test the interface of all module classes
 import Events
 import Models
 import unittest
 
 class TestModels(unittest.TestCase):
-  def runTest(self):
-    self.model = Model.Model("zero")
+
+  def testModel(self):
+    self.model = Models.Model("model_0")
     self.model.getId()
     self.model.notify( Events.Event() ) 
+
 
   
