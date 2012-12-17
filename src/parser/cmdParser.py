b778b20957ef7779b09559e7333d0477a6484483
Kyle <ksnavely@localhost.localdomain>
2012-12-17 10:55:32 -0500

    Parser not quite meshed yet, working on making parser objects

--- src/parser/cmdParser.py
+++ src/parser/cmdParser.py
@@ -1,25 +1,26 @@
-
 import os, sys
+
 lib_path = os.path.abspath('..')
 sys.path.append(lib_path)
 
-if sys.version_info[0] >= 3:
-    raw_input = input
+import ply.lex as lex
+import ply.yacc as yacc
+import base.Events as Events
 
 reserved = { 
-    'say' : 'SAY',
-    }
+  'say' : 'SAY',
+  }
 
 tokens = [
-    'STRING'
-    ] + list(reserved.values())
+  'STRING'
+  ] + list(reserved.values())
 
 # Tokens
 
 def t_STRING(t):
-   r'[a-zA-Z_][a-zA-Z0-9_]*'
-   t.type = reserved.get(t.value,'STRING')
-   return t
+  r'[a-zA-Z_][a-zA-Z0-9_]*'
+  t.type = reserved.get(t.value,'STRING')
+  return t
 
 t_ignore = " \t"
 
@@ -31,8 +32,6 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
     
-# Build the lexer
-import ply.lex as lex
 lex.lex()
 
 # Parsing rules
@@ -44,7 +43,11 @@ precedence = (
 
 def p_expression_say(p):
     "expression : SAY expression"
-    print(p[2])
+    """
+    mvEvent = Events.LogMsgEvent(str( p[2] ))
+    self.appCollection.notify( mvEvent )
+    """
+    print( p[2] )
 
 def p_words_expression(p):
     "expression : expression STRING"
@@ -60,9 +63,17 @@ def p_error(p):
     else:
         print("Syntax error at EOF")
 
-import ply.yacc as yacc
 yacc.yacc()
 
 def parse(s):
+  if not s: return
+  yacc.parse(s)
+ 
+def parseLoop():
+  while True:
+    s = input("kadv: ")
     if not s: return
     yacc.parse(s)
+
+if __name__ == "__main__":
+  parseLoop()
