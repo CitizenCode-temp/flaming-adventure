569074ed2df370cf0fec653b42de137305dfe14a
Kyle Snavely <ksnavely@cloudant.com>
2013-12-25 18:54:26 -0500

    restructure app

--- adv/__init__.py
+++ adv/__init__.py
@@ -29,4 +29,3 @@ class App:
     return True
 
 app = App()
-curses.wrapper( app.run )
---
+++ start.py
@@ -0,0 +1,9 @@
+import adv
+
+import curses
+
+
+if __name__ == '__main__':
+    curses.wrapper(adv.app.run)
+
+    
